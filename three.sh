fd459e545ec6cdbdac0f5b64b4280a4139ac9ec4
$MY_NAME <komuw05@gmail.com>
2022-03-25 12:31:14 +0300

    g

--- three.sh
+++ three.sh
@@ -7,7 +7,7 @@ shopt -s nullglob globstar
 # K8s management:
 # - Intro to high availability.
 # - Intro to k8s management tools.
-# - Safely drainig a k8s node.
+# - Safely draining a k8s node.
 # - Upgrading k8s with kubeadm.
 # - Backing up & restoring etcd cluster data.
 
@@ -28,13 +28,88 @@ shopt -s nullglob globstar
 #  (a) Stacked; etcd runs in each of those control-plane nodes. kubeadm uses this.
 #  (b) External; etcd running in external server/s.
 
+# K8s management tools.
+#  (a) kubectl; official cli for k8s.
+#  (b) kubeadm; easily create clusters.
+#  (c) minikube; setup k8s cluster using one server/machine.
+#  (d) helm; templating & package management for k8s objects.
+#  (e) kompose; transtion from docker/docker-compose to k8s.
+#  (f) kustomize; cfg management tool for managing k8s object configs. Kind a bit like helm.
 
+# Safely draining a node.
+# Drain: Remove a k8s node from service. eg during maintenance.
+#        containers are terminated and/or rescheduled in other nodes.
+#        You may need to ignore Daemonsets(pods that are tied to each node.)
+#        You may also need to ignore any stand-alone pods(that aren't part of a deployment/replicaset etc) using `--force` flag
+# Uncordon: Allow pods to run on the node after maintenance is complete.
+#           uncordon does not cause already running pods to be rescheduled, but newly created pods can be scheduled in the node.
+kubectl get nodes
+kubectl drain <node-name> --ignore-daemonsets --force
+kubectl uncordon <node-name>
 
+# Upgrading k8s.
+# - control plane steps.
+# - worker node steps.
+# 
+# (a) control-plane upgrade.
+#     - drain node
+#     - upgrade kubeadm
+#     - plan upgrade
+#     - apply upgrade
+#     - upgrade kubelet & kubectl
+#     - uncordon node.
+#
+# (b) worker upgrade.
+#     - drain node
+#     - upgrade kubeadm
+#     - upgrade kubelet configuration
+#     - upgrade kubelet & kubectl
+#     - uncordon node.
 
+upgrade_control_plane(){
+    control_plane_name=$1
 
+    kubectl get nodes
+    kubectl drain "${control_plane_name}" --ignore-daemonsets
+    sudo apt -y update
+    kubeadm version
+    sudo apt -y install --allow-change-held-packages kubeadm==1.22.2-00
+    kubeadm version
+    sudo kubeadm upgrade plan v1.22.2
+    sudo kubeadm upgrade apply v1.22.2
+    kubelet version
+    kubectl version
+    sudo apt -y install --allow-change-held-packages kubelet=1.22.2-00 kubectl=1.22.2-00
+    kubelet version
+    kubectl version
+    sudo systemctl daemon-reload
+    sudo systemctl restart kubelet
+    kubectl uncordon "${control_plane_name}"
+    sleep 3
+    kubectl get nodes
+}
+upgrade_control_plane k8s-control
 
+upgrade_worker(){
+    worker_name=$1
 
-
+    kubectl get nodes
+    kubectl drain "${worker_name}" --ignore-daemonsets # this specific command should be ran in control-plane.
+    sudo apt -y update
+    kubeadm version
+    sudo apt -y install --allow-change-held-packages kubeadm==1.22.2-00
+    kubeadm version
+    sudo kubeadm upgrade node
+    kubelet version
+    kubectl version
+    sudo apt -y install --allow-change-held-packages kubelet=1.22.2-00 kubectl=1.22.2-00
+    kubelet version
+    kubectl version
+    sudo systemctl daemon-reload
+    sudo systemctl restart kubelet
+    kubectl uncordon "${worker_name}" # this specific command should be ran in control-plane.
+}
+upgrade_worker k8s-worker1
 
 
 
