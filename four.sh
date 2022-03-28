ae3534c9a128f408829930655cdcf1c929e13cff
$MY_NAME <komuw05@gmail.com>
2022-03-28 11:23:38 +0300

    g

---
+++ four.sh
@@ -0,0 +1,74 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+
+## chapter 4.
+# Working with kubectl: https://kubernetes.io/docs/reference/kubectl/cheatsheet/
+#  - kubectl get           : list objects in cluster.
+#  - kubectl describe      : detailed info about objects.
+#  - kubectl create        : create objects. -f <file-name>. if object exists it errors.
+#  - kubectl apply         : similar to create, if object exists it updates.
+#  - kubectl delete
+#  - kubectl exec          : run command inside container.
+#  - kubectl api-resources :lists all the resources/objects in a cluster. This resources can then be used in `kubectl get <resource>`
+
+kubectl_cheatsheet(){
+    # https://kubernetes.io/docs/reference/kubectl/cheatsheet/
+    # lists all the resources/objects in a cluster. This resources can then be used in `kubectl get <resource>`
+    kubectl api-resources
+
+    # get pods/services
+    kubectl get services --all-namespaces
+    kubectl get pods --all-namespaces
+
+    # describe a pod
+    kubectl describe pod --namespace=ara-dev controller-ara-69d7876896-pml5q
+
+    # fetch logs(selecting by labels)
+    kubectl logs -f --namespace=ara-dev -l app.kubernetes.io/instance=ara --all-containers=true --max-log-requests=21
+    kubectl logs -f --namespace=ara-dev -l app.kubernetes.io/component=billing --all-containers=true
+    kubectl logs -f --namespace=ara-dev -l app.kubernetes.io/component=controller --all-containers=true
+    kubectl logs -f --namespace=ara-dev -l app.kubernetes.io/component=dashboard --all-containers=true
+
+    # exec into a pod
+    kubectl exec --stdin --tty my-pod -- /bin/sh                  # 1 container case
+    kubectl exec --stdin --tty my-pod -c my-container -- /bin/sh  # multi-container case
+    kubectl exec --namespace=namespace --stdin --tty podName -- /bin/sh
+
+    # show resource usages
+    kubectl top pod --all-namespaces
+    kubectl top node
+
+    kubectl delete pod --namespace=hey my_pod
+
+    # persistent volumes & persistent-volume-claims: https://kubernetes.io/docs/concepts/storage/persistent-volumes/
+    kubectl get pv                                                                                 
+    kubectl get pvc --all-namespaces
+    kubectl describe pv <name>
+    kubectl describe pvc --namespace=namespace <name>
+
+    # secrets
+    kubectl get secrets --all-namespaces
+    kubectl describe secret --namespace=namespace <secret_name>
+    kubectl get secret --namespace=foobarbaz-bingbong ara-home -o yaml
+    kubectl get secret --namespace=foobarbaz-bingbong ara-home --template={{.data.mongoCS}} | base64 -d
+
+    # networkPolicy
+    kubectl get networkpolicy --all-namespaces
+    kubectl get networkpolicy --namespace=foobarbaz-bingbong <name>
+    kubectl describe networkpolicy --namespace=foobarbaz-bingbong <name>
+
+    # create a pod for testing. This one has ping,telnet,wget etc already installed.
+    # Note; you can specify labels, namespace etc.
+    # eg for this case, we run it in kube-system namespace bcoz we want to test wether we can ping a pod from the kube-system NS
+    kubectl run \
+    --namespace=kube-system \
+    --labels="app=tester-pod,env=dev,country=kenya" \
+    tester-pod \
+    --rm -ti \
+    --image busybox \
+    /bin/sh
+}
+
+
