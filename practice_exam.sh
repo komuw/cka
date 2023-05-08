611c5ed4f15e83d5bb404bd77c903f9359275768
$MY_NAME <komuw05@gmail.com>
2023-05-08 10:18:13 +0300

    continue practice exams

--- practice_exam.sh
+++ practice_exam.sh
@@ -198,3 +198,192 @@ part_four(){
 
     ./verify.sh
 }
+
+# 5. part 5.
+# (a) Upgrade all components on the control plane node to Kubernetes version 1.22.2.
+# (b) Upgrade all components on the two worker nodes to Kubernetes version 1.22.2.
+part_five(){
+    kubectl config use-context acgk8s # Switch to the `acgk8s` k8s cluster.
+
+    # (a)
+    sudo apt -y update
+    kubectl get nodes
+    kubectl drain <control_plane_name> --ignore-daemonsets
+    sudo apt -y install --allow-change-held-packages kubeadm=1.22.2-00
+    sudo kubeadm upgrade plan v1.22.2
+    sudo kubeadm upgrade apply v1.22.2
+    sudo apt -y install --allow-change-held-packages kubelet=1.22.2-00 kubectl=1.22.2-00
+    sudo systemctl daemon-reload
+    sudo systemctl restart kubelet
+    kubectl uncordon <control_plane_name>
+
+    # (b)
+    kubectl drain acgk8s-worker1 --ignore-daemonsets --force # Should be ran in control-plane.
+    ssh acgk8s-worker1
+    sudo apt -y update
+    sudo apt -y install --allow-change-held-packages kubeadm=1.22.2-00
+    sudo kubeadm upgrade node
+    sudo apt -y install --allow-change-held-packages kubelet=1.22.2-00 kubectl=1.22.2-00
+    sudo systemctl daemon-reload
+    sudo systemctl restart kubelet
+    exit # to get out of ssh and go back to cp node.
+    kubectl uncordon acgk8s-worker1 # Should be ran in control-plane.
+
+    kubectl drain acgk8s-worker2 --ignore-daemonsets --force # Should be ran in control-plane.
+    ssh acgk8s-worker2
+    sudo apt -y update
+    sudo apt -y install --allow-change-held-packages kubeadm=1.22.2-00
+    sudo kubeadm upgrade node
+    sudo apt -y install --allow-change-held-packages kubelet=1.22.2-00 kubectl=1.22.2-00
+    sudo systemctl daemon-reload
+    sudo systemctl restart kubelet
+    exit # to get out of ssh and go back to cp node.
+    kubectl uncordon acgk8s-worker2 # Should be ran in control-plane.
+
+    ./verify.sh
+}
+
+# 6. part 6.
+# (a) Drain Worker Node 1(acgk8s-worker1)
+# (b) Create a Pod That Will Only Be Scheduled on Nodes with a Specific Label
+#     Add the disk=fast label to the acgk8s-worker2 Node.
+#     Create a pod called fast-nginx(nginx image) in the dev namespace that will only run on nodes with this label.
+part_six(){
+    kubectl config use-context acgk8s # Switch to the `acgk8s` k8s cluster.
+
+    kubectl drain acgk8s-worker1 # This will give error, and also tell you which flags u need to add.
+    kubectl drain acgk8s-worker1 --ignore-daemonsets --force --delete-local-data
+
+    kubectl label nodes acgk8s-worker2 disk=fast
+    pod_contents="
+apiVersion: v1
+kind: Pod
+metadata:
+  name: fast-nginx
+  namespace: dev
+spec:
+  nodeSelector:
+    disk: fast
+  containers:
+  - name: nginx
+    image: nginx
+"
+
+    ./verify.sh
+}
+
+# 7. part 7.
+# (a) Create a PersistentVolume
+#     It will be called `host-storage-pv` with storage capacity 1Gi in the acgk8s context.
+#     Configure it so that volumes that use it can be expanded in the future.
+#     Note: This may require you to create additional objects.
+# (b) Create a Pod That Uses the PersistentVolume for Storage
+#     Pod called `pv-pod` in the `auth`` Namespace. It should use the `host-storage-pv` PersistentVolume for storage.
+#     This will require a PVC `host-storage-pvc` with size 100Mi in same namespace.
+#     Mount the volume that uses the PV for storage, so that `/output` directory ultimately writes to the PV.
+# (c) Expand the PersistentVolumeClaim so that it requests 200Mi.
+part_seven(){
+    kubectl config use-context acgk8s # Switch to the `acgk8s` k8s cluster.
+
+    sc="
+apiVersion: storage.k8s.io/v1
+kind: StorageClass
+metadata:
+  name: localdisk
+provisioner: kubernetes.io/no-provisioner # store on host disk.
+allowVolumeExpansion: true # VERY important.
+"
+    pv="apiVersion: v1
+kind: PersistentVolume
+metadata:
+  name: host-storage-pv
+spec:
+  storageClassName: localdisk
+  persistentVolumeReclaimPolicy: Recycle
+  capacity:
+    storage: 1Gi
+  accessModes:
+    - ReadWriteOnce
+  hostPath:
+    path: /etc/data
+"
+    pvc="apiVersion: v1
+kind: PersistentVolumeClaim
+metadata:
+  name: host-storage-pvc
+  namespace: auth
+spec:
+  storageClassName: localdisk
+  accessModes:
+    - ReadWriteOnce
+  resources:
+    requests:
+      storage: 100Mi # For part (c) of the question, edit this and use 200Mi 
+"
+    pod="apiVersion: v1
+kind: Pod
+metadata:
+  name: pv-pod
+  namespace: auth
+spec:
+  containers:
+  - name: busybox
+    image: busybox
+    command: ['sh', '-c' 'while true; do echo success > /output/output.log; sleep 5; done']
+    volumeMounts:
+    - name: pv-storage
+      mountPath: /output # directory inside the container where volume is at.
+  volumes:
+  - name: pv-storage
+    persistentVolumeClaim:
+      claimName: host-storage-pvc # Should match the pvc.metadata.name
+"
+
+    ./verify.sh
+}
+
+# 8. part 8.
+# (a) Create a networkPolicy that denies all access to the `maintenance` pod in the `foo` namespace.
+# (b) Create a networkPolicy that allows ALL pods in the `users-backend` namespace to communicate with each other only on port 80.
+part_eight(){
+    kubectl config use-context acgk8s # Switch to the `acgk8s` k8s cluster.
+
+    kubectl describe pod maintenance -n foo # Look at the labels it has.
+    lockdown_np_contents="
+apiVersion: networking.k8s.io/v1
+kind: NetworkPolicy
+metadata:
+  name: my-networkpolicy
+  namespace: foo
+spec:
+  podSelector:
+    matchLabels:
+      app: maintenance # This is one of the labels we got from running `kubectl describe pod maintenance -n foo`.
+  policyTypes:
+  - Ingress
+  - Egress
+"
+
+    kubectl label namespace users-backend app=users-backend # Label the NS to make it easier to do things with it.
+    allow_np_contents="
+apiVersion: networking.k8s.io/v1
+kind: NetworkPolicy
+metadata:
+  name: my-networkpolicy
+  namespace: users-backend
+spec:
+  podSelector: {} # Leaving it empty will match all pods in the `users-backend` NS.
+  policyTypes:
+  - Ingress # This NP does not apply to `Egress`
+  ingress:
+  - from:
+    - namespaceSelector:
+        matchLabels:
+          name: users-backend # Allow traffic from any pods in the `users-backend` NS to the pods in same NS at port 80.
+    ports:
+    - protocol: TCP
+      port: 80
+"
+
+    ./verify.sh
+}
